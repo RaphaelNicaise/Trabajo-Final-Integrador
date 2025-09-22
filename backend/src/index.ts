295e1ca9d6e66f90593cb7552e8e311f40b65a95
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-22 06:11:04 -0300

    Integracion de API MP, ya se crean preferences

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import express from 'express';
+import paymentsRoutes from "./routes/payments";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Backend corriendo 🚀');
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Backend MercadoPago corriendo 🚀'));
+
+app.use("/payments", paymentsRoutes);
+
+app.get('/success', (req, res) => {
+  res.send('<h1>¡Pago exitoso!</h1>');
+});
+
+app.get('/failure', (req, res) => {
+  res.send('<h1>Pago fallido</h1>');
+});
+
+app.get('/pending', (req, res) => {
+  res.send('<h1>Pago pendiente</h1>');
 });
 
 app.listen(PORT, () => {
