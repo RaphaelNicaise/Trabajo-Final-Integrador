58c8a1a219cafa5c3693bcd4632b931e1c38ced2
RaphaelNicaise <raphanicaise@gmail.com>
2026-03-04 13:19:20 -0300

    Rate Limit de auth aumentado

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -34,7 +34,7 @@ export const initializeRateLimiters = () => {
 
   authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 20,
+    max: 60,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
